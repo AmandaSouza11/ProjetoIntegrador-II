ce45aa6330dda1a839d6b30c8c5bda88c725b51d
AmandaSouza11 <mandy.ss1105@gmail.com>
2024-09-17 21:11:13 -0300

    criacao do data

---
+++ src/main/java/com/example/projeto_integrador/data/MedicoData.java
@@ -0,0 +1,23 @@
+package com.example.projeto_integrador.data;
+
+import lombok.Data;
+
+@Data
+public class MedicoData {
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String ra;
+    private String crm;
+    private String crn;
+    private String cep;
+    private String bairro;
+    private String rua;
+    private String numero_residencial;
+    private String complemento;
+    private String cidade;
+    private String especialidade;
+    
+}
---
+++ src/main/java/com/example/projeto_integrador/data/PacienteData.java
@@ -0,0 +1,21 @@
+package com.example.projeto_integrador.data;
+
+import lombok.Data;
+
+@Data
+public class PacienteData {
+
+    private String nome;
+    private String data_nascimento;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String cep;
+    private String bairro;
+    private String rua;
+    private String numero_residencial;
+    private String complemento;
+    private String cidade;
+    
+}
